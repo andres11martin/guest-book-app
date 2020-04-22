a678831c3ca0a8f25d4e5abb89aa24c805097e54
andres93martin <andresmartincorrea@gmail.com>
2020-04-22 16:29:00 -0300

    order

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,7 +7,6 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    console.log("holaaaaaaa", req.body)
     const post = await Post.create(req.body)
     console.log(post)
     return res.json(post)
---
+++ web/pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Head from '../components/head';
+import Nav from '../components/nav';
+
+const App = function () {
+  (
+
+    <h1>Hello world!</h1>
+  )
+
+
+};
+
+export default App
